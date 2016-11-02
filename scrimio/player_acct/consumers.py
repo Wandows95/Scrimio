b211d23f467f11ad502d1054f726be8a22fddef4
Austin Matthews <amatthews11@mail.csuchico.edu>
2016-11-02 13:09:03 -0700

    Scrimio: Added friendslist consumers. Dota_mm: Modified order of JSON fields in ws_ready_status_update()

--- scrimio/player_acct/consumers.py
+++ scrimio/player_acct/consumers.py
@@ -4,4 +4,33 @@ from .models import User
 def ws_login(message):
 	user = get_object_or_404(User, user=message.user)
 	user.is_online = True
-	user.save
+	user.save()
+
+	# Join your own sub feed
+	Group("feed-%s" % user.username).add(message.reply_channel)
+
+	# Subscribe to friends list feed
+	for player in user.friends.all():
+		Group("feed-%s" % player.username).add(message.reply_channel)
+
+@channel_session
+def ws_status_update(message):
+	data = json.loads(message['data'])
+	status = data['status']
+	username = data['username']
+
+	# Broadcast user status
+	Group("feed-%s" % username).send({'username':username, 'status': status})
+
+@channel_session
+def ws_logout(message):
+	user = get_object_or_404(User, user=message.user)
+	user.is_online = False
+	user.save()
+
+	# Leave your own sub feed
+	Group("feed-%s" % user.username).discard(message.reply_channel)
+
+	# Unsubscribe from friends list feed
+	for player in user.friends.all():
+		Group("feed-%s" % player.username).discard(message.reply_channel)
